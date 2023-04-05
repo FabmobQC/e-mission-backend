144428501592e82beb80117f98aa4b571564f43a
yattdev <yattdeveloper@gmail.com>
2023-04-05 16:02:03 +0000

    Added inlines form for ProjectMode and ProjectPurposeinto ProjectAdmin

--- emissionbackend/projects/admin.py
+++ emissionbackend/projects/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 
-from projects.models import Project, Form, FormURL, Notification, NotificationTitles, NotificationMessages
+from projects.models import Project, Form, FormURL, Notification, NotificationTitles, NotificationMessages, ProjectMode, ProjectPurpose
 from projects.models import Mode, ModeTranslation
 from projects.models import Purpose, PurposeTranslation
 from projects.models import Email, EmailSubjects, EmailMessages
 
-admin.site.register(Project)
+
+class ProjectModeInline(admin.TabularInline):
+    model = ProjectMode
+    extra = 1
+
+
+class ProjectPurposeInline(admin.TabularInline):
+    model = ProjectPurpose
+    extra = 1
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = (ProjectModeInline, ProjectPurposeInline)
+
+
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Form)
 admin.site.register(FormURL)
 admin.site.register(Notification)
