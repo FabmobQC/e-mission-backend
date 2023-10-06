a519da9456bf959fbb3ef8018865d18151c167e7
yattdev <yattdeveloper@gmail.com>
2023-10-06 22:15:49 +0100

    UPdate app api to handle user management

--- emissionbackend/api/urls.py
+++ emissionbackend/api/urls.py
@@ -1,9 +1,33 @@
-from django.conf.urls import url
-from api import views
+from django.urls import re_path
+from django.urls.conf import include
+from api import views as api_views
+from userprofile import views as user_views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', user_views.UserViewSet, basename="users")
 
 urlpatterns = [
-    url(r'^projects/$', views.getProjects),
-    url(r'^projects/([0-9]+)$', views.getProject),
-    url(r'^userprofile/$', views.UserProfileAPI),
-    url(r'^projectusers/([0-9]+)$', views.ProjectUsersAPI)
+    re_path(
+        r'^projects/$',
+        api_views.getProjects
+    ),
+    re_path(
+        r'^projects/([0-9]+)$',
+        api_views.getProject
+    ),
+    re_path(
+        r'^projectusers/([0-9]+)$',
+        api_views.ProjectUsersAPI
+    ),
+    re_path('^login/',
+            user_views.LoginView.as_view(),
+            name='login'
+            ),
+    re_path('^logout/',
+            user_views.LogoutView.as_view(),
+            name='logout'
+            ),
+
+    re_path('', include(router.urls)),
 ]
