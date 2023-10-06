5523ddf6c893534bd40e10067d25d5b872007b29
yattdev <yattdeveloper@gmail.com>
2023-10-06 15:50:30 +0100

    Update users display on admin panel

--- emissionbackend/userprofile/admin.py
+++ emissionbackend/userprofile/admin.py
@@ -6,5 +6,9 @@ from userprofile.models import User
 
 @admin.register(User)
 class UserAdmin(AuthUserAdmin):
-    list_display = ('email', 'username',
-                    'project', 'is_active', 'is_superuser')
+    list_display = ('email', 'get_projects', 'is_active', 'is_superuser')
+    ordering = ('email',)
+
+    @admin.display(description='Projects')
+    def get_projects(self, obj):
+        return "|".join([p.id for p in obj.projects.all()])
