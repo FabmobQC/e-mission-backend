fbff569e539788caffd846393a32f4cf17737624
yattdev <yattdeveloper@gmail.com>
2023-10-06 17:26:19 +0100

    Update fieldsets from UserAdmin

--- emissionbackend/userprofile/admin.py
+++ emissionbackend/userprofile/admin.py
@@ -6,8 +6,30 @@ from userprofile.models import Server, User
 
 @admin.register(User)
 class UserAdmin(AuthUserAdmin):
-    list_display = ('email', 'get_projects', 'is_active', 'is_superuser')
+    list_display = ('token', 'email', 'get_projects',
+                    'is_active', 'is_superuser')
     ordering = ('email',)
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:  # add_fieldsets
+            fieldsets = (
+                ("Authentication", {
+                 'fields': ('email', 'password1', 'password2',)}),
+                ("Personal ino", {
+                 'fields': ('first_name', 'last_name', 'server',)}),
+            )
+        else:
+            fieldsets = (
+                ("Authentication",
+                    {'fields': ('email', 'password',)}),
+                ("Personal ino",
+                    {'fields': ('first_name', 'last_name', 'server',)}),
+                ("Permissions", {
+                 'fields': ('groups', 'user_permissions',)}),
+                ("Activity", {
+                 'fields': ('is_active', 'last_login', 'date_joined',)}),
+            )
+        return fieldsets
 
     @admin.display(description='Projects')
     def get_projects(self, obj):
